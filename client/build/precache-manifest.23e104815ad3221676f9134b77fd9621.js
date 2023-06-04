4edac170f7d5d10ea5a7105923cb40492d1b2bfe
unknown <gldhperes@gmail.com>
2023-06-04 12:51:52 -0300

    added a 'build' on package

---
+++ client/build/precache-manifest.23e104815ad3221676f9134b77fd9621.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3a56e67eccb79d17819543885a37bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c87fe0d06c9dec8c6f7",
+    "url": "/static/css/main.901e1272.chunk.css"
+  },
+  {
+    "revision": "a8f921a4db6ed066174f",
+    "url": "/static/js/2.a18fc1f1.chunk.js"
+  },
+  {
+    "revision": "fcad5adb7a72840a9b3286e1bf2f627e",
+    "url": "/static/js/2.a18fc1f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c87fe0d06c9dec8c6f7",
+    "url": "/static/js/main.1392ea3f.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.1392ea3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "5c0c63fb62ec323a10598d7e9de4609e",
+    "url": "/static/media/memories.5c0c63fb.png"
+  }
+]);
