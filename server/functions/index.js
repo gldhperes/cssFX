2d80259790b3d65c2c3ad68193c850a4b12e2a7c
unknown <gldhperes@gmail.com>
2023-06-04 21:39:51 -0300

    tentativa de deploy 9

--- server/functions/index.js
+++ server/functions/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
 
 app.use(cors({ 
-    origin: ['*'],
+    origin: '*',
     methods: 'GET, POST, PUT, DELETE, PATCH',
     allowedHeaders: 'Content-Type, Authorization'
   }));
