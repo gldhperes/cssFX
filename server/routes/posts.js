594f7ea842804683dce69d9c09f834a1673effbf
unknown <gldhperes@gmail.com>
2023-06-04 22:00:00 -0300

    tentativa de deploy 12

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -6,7 +6,6 @@ import auth from "../middleware/auth.js";
 const router = express.Router()
 
 router.get('/', getPosts),
-router.get('/posts', getPosts),
 router.get('/search', getPostsBySearch) 
 router.get('/:id', getPost) 
 
